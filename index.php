3501c3f9b7ff3add5d1156237e39a605dc6d9d4a
David Coello Suárez <51972354+David-Coello-Suarez@users.noreply.github.com>
2022-07-05 00:14:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (file_exists("config.php")) {
 $conexion = new Conexion();
 $conexion->DBConexion();
 
-// OBTENER PARAMETROS
+// OBTENER PARAMETROS 2
 $sql = $conexion->DBConsulta("SELECT * FROM Parametros", false, array(1));
 $parametro = array();
 foreach ($sql as $fila) {
